1e98059fca94d25f5ce260dbc35fa3c0354d25d5
bs <bsantraigi@gmail.com>
2023-06-04 21:17:27 +0530

    updates

--- launcher.py
+++ launcher.py
@@ -14,14 +14,14 @@ def cmdline_args(x=None):
     --wandb: Use wandb or not
     """
     parser = argparse.ArgumentParser(description='Launcher')
-    parser.add_argument('-d', '--dataset', type=str, default='MSC', help='Dataset to use (MSC or TC)')
-    parser.add_argument('-m', '--model', type=str, default='flan-t5', help='Model to use (flan-t5, tk-instruct, T0)')
-    parser.add_argument('-pt','--prompt_type',  type=str, default='manual', help='manual or ppl')
+    parser.add_argument('-d', '--dataset', type=str, default='MSC', help='Dataset to use (MSC or TC)', choices=["MSC", "TC"])
+    parser.add_argument('-m', '--model', type=str, default='flan-t5', help='Model to use (flan-t5, tk-instruct, T0)', choices=["flan-t5", "T0", "tk-instruct"])
+    parser.add_argument('-pt','--prompt_type',  type=str, default='manual', help='manual or ppl', choices=["manual", "ppl"])
     parser.add_argument('-fs','--few_shot',  action="store_true", help='Use 1 example per along with each query, otherwise zero_shot')
     parser.add_argument('-bw','--background_knowledge',  action="store_true", help='Use background knowledge or not (in summarized format)')
     parser.add_argument('-hst','--history_signal_type',  type=str, default='full', choices=['full', 'peg', 'bart', 'recent-k', 'semantic-k', 'none'],
                         help='Type of history signal (full, peg, bart, recent-k, semantic-k, none)')
-    parser.add_argument('-hk','--history_k',  type=int, default=1, help='Number of utterances to use in recent-k or semantic-k')
+    parser.add_argument('-hk','--history_k',  type=int, default=4, help='Number of utterances to use in recent-k or semantic-k', choices=[2,4,8,10])
     parser.add_argument('-w','--wandb',  action="store_true", help='Use wandb or not')
     if x is not None:
         return parser.parse_args(x)
@@ -29,14 +29,16 @@ def cmdline_args(x=None):
         return parser.parse_args()
     
 def main():
-    args = cmdline_args(["-w"])
+    args = cmdline_args()
     print(args)
     arg_dict = vars(args)
     if args.wandb:
         wandb.init(project="frugal-prompts", config=arg_dict)
 
+    dataset = load_dataset(args.dataset, args.)
+    
     # Test: report some random results to wandb for now
-    if args.wandb:
+    if args.wandb:        
         # BLEU	METEOR	ROUGE-1	ROUGE-2	ROUGE-L	BERTScore-p	BERTScore-r	BERTScore-F1	DEB	BLEURT	prompt_len output_len
         wandb.log({
             "BLEU": random.random(),
