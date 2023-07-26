9389aae913e30dc9f2d7f39b3b8ca3fad8a0544f
bishal <bsantraigi@gmail.com>
2023-07-26 16:58:32 +0530

    add OnlyBI run files

--- data/cmds_superset.sh
+++ data/cmds_superset.sh
@@ -474,3 +474,31 @@ python launcher.py -d TC -m dv3 -pt manual   -hst semantic-k -hk 1
 python launcher.py -d TC -m dv3 -pt manual --few_shot  -hst peg_cd
 python launcher.py -d TC -m dv3 -pt manual --few_shot  -hst recent-k -hk 1
 python launcher.py -d TC -m dv3 -pt manual --few_shot  -hst semantic-k -hk 1
+python launcher.py -d MSC -m flan-t5 -pt manual  --background_knowledge -hst none
+python launcher.py -d MSC -m flan-t5 -pt manual --few_shot --background_knowledge -hst none
+python launcher.py -d MSC -m flan-t5 -pt ppl  --background_knowledge -hst none
+python launcher.py -d MSC -m flan-t5 -pt ppl --few_shot --background_knowledge -hst none
+python launcher.py -d MSC -m T0 -pt manual  --background_knowledge -hst none
+python launcher.py -d MSC -m T0 -pt manual --few_shot --background_knowledge -hst none
+python launcher.py -d MSC -m T0 -pt ppl  --background_knowledge -hst none
+python launcher.py -d MSC -m T0 -pt ppl --few_shot --background_knowledge -hst none
+python launcher.py -d MSC -m tk-instruct -pt manual  --background_knowledge -hst none
+python launcher.py -d MSC -m tk-instruct -pt manual --few_shot --background_knowledge -hst none
+python launcher.py -d MSC -m tk-instruct -pt ppl  --background_knowledge -hst none
+python launcher.py -d MSC -m tk-instruct -pt ppl --few_shot --background_knowledge -hst none
+python launcher.py -d TC -m flan-t5 -pt manual  --background_knowledge -hst none
+python launcher.py -d TC -m flan-t5 -pt manual --few_shot --background_knowledge -hst none
+python launcher.py -d TC -m flan-t5 -pt ppl  --background_knowledge -hst none
+python launcher.py -d TC -m flan-t5 -pt ppl --few_shot --background_knowledge -hst none
+python launcher.py -d TC -m T0 -pt manual  --background_knowledge -hst none
+python launcher.py -d TC -m T0 -pt manual --few_shot --background_knowledge -hst none
+python launcher.py -d TC -m T0 -pt ppl  --background_knowledge -hst none
+python launcher.py -d TC -m T0 -pt ppl --few_shot --background_knowledge -hst none
+python launcher.py -d TC -m tk-instruct -pt manual  --background_knowledge -hst none
+python launcher.py -d TC -m tk-instruct -pt manual --few_shot --background_knowledge -hst none
+python launcher.py -d TC -m tk-instruct -pt ppl  --background_knowledge -hst none
+python launcher.py -d TC -m tk-instruct -pt ppl --few_shot --background_knowledge -hst none
+python launcher.py -d MSC -m dv3 -pt manual  --background_knowledge -hst none
+python launcher.py -d MSC -m dv3 -pt manual --few_shot --background_knowledge -hst none
+python launcher.py -d TC -m dv3 -pt manual  --background_knowledge -hst none
+python launcher.py -d TC -m dv3 -pt manual --few_shot --background_knowledge -hst none
